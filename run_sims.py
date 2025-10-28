ecf6c08bec4462d3a846cb6f46af6e5016d2c94d
Robyn Stuart <robyna.s@gmail.com>
2025-10-28 12:15:25 +1100

    calib

--- run_sims.py
+++ run_sims.py
@@ -18,7 +18,7 @@ debug = 0  # Run with smaller population sizes and in serial
 do_shrink = True  # Do not keep people when running sims (saves memory)
 
 # Run settings
-n_trials    = [3000, 2][debug]  # How many trials to run for calibration
+n_trials    = [2500, 2][debug]  # How many trials to run for calibration
 n_workers   = [50, 1][debug]    # How many cores to use
 storage = None
 
